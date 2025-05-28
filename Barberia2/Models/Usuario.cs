ef67714b9546c2425c7af51efba57284d9995921
FabioRamirezMoreno10Ne <Fabio.Ramirez@cecar.edu.co>
2025-05-28 03:00:24 -0500

    ﻿commit de usuario

---
+++ Barberia2/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barberia2.Models
+{
+    public class Usuario
+    {
+        public string Email { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public int ClienteId { get; set; }
+        public string Contrasenia { get; set; }
+        public string Estado { get; set; } = "Activo";
+
+        public Usuario()
+        {
+            FechaRegistro = DateTime.Now;
+            Estado = "Activo";
+        }
+    }
+}
